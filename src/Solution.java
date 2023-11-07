570b3a6acc48f53a3b9f1647a835b4b5e1635e9f
Nam Nguyen <nguyentnam.061297@gmail.com>
2023-11-07 21:43:47 +0700

    123

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,10 @@
+import com.sun.source.tree.ReturnTree;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Solution {
 
     //1. Merge Sort
@@ -134,5 +141,116 @@ public class Solution {
             }
         }
         return !left && right;
+    }
+
+    public boolean isHappy(int n) {
+        Map<Integer, Integer> arr = new HashMap<>(); //tạo 1 mảng HashMap
+        if (n < 1 || n > Math.pow(2, 31) - 1) return false; //check đkiện n theo đề bài ban đầu
+        return recursion(n, arr);
+    }
+
+    public boolean recursion(int n, Map<Integer, Integer> arr) { //dùng đệ quy để loop liên tục với số sau khi return
+        String nStr = Integer.toString(n);  //convert số nguyên n sang kiểu String
+        int sum = 0;
+        for (int i = 0; i < nStr.length(); i++) {   //duyệt for cho từng ký tự chữ số
+            sum += Math.pow(nStr.charAt(i) - '0', 2);   //mỗi lần duyệt sẽ + với số index thứ i mũ 2, trừ 0 là vì theo bảng mã ASCII
+        }
+        if (sum == 1) return true;
+        else if (sum < 1 || sum > Math.pow(2, 31) - 1) return false;    //check đkiện đề bài
+        else if (arr.containsKey(sum))
+            return false;    //nếu sum đã tồn tại trong mảng arr trước đó tức loop vô hạn => return false
+        arr.put(sum, sum);  //nếu chưa tồn tại && khác 1 thì put vào mảng arr
+        return recursion(sum, arr);
+    }
+
+    public boolean isValidSudoku(char[][] board) {
+        boolean row = true;
+        boolean column = true;      //char[hàng][cột]
+        int i = 0;
+        int j = 0;
+        Map<Character, Integer> arrRow = new HashMap<>();
+        Map<Character, Integer> arrCol = new HashMap<>();
+        Map<Character, Integer> arrSubBox = new HashMap<>();
+        return isValidRow(board, arrRow) && isValidCol(board, arrCol) && checkSubBox(board, arrSubBox);
+    }
+
+    //    public boolean isValidRow(char[][] board, Map<Character, Integer> arrRow, int i, int j) {
+//        if (j == board[0].length) {
+//            i++;
+//            j = 0;
+//            arrRow.clear();
+//        }
+//        if (i == board[0].length) return true;
+//        if (board[i][j] == '.') {
+//            j++;
+//            return isValidRow(board, arrRow, i, j);
+//        }
+//        if (arrRow.containsKey(board[i][j]) && board[i][j] != '.') return false;
+//        else if (!arrRow.containsKey(board[i][j]) && board[i][j] != '.'){
+//            arrRow.put(board[i][j], j);
+//            j++;
+//        }
+//        return isValidRow(board, arrRow, i, j);
+//    }
+    public boolean isValidRow(char[][] board, Map<Character, Integer> arrRow) {
+        for (int i = 0; i < 9; i++) {
+            arrRow.clear();
+            for (int j = 0; j < 9; j++) {
+                if (arrRow.containsKey(board[i][j]) && board[i][j] != '.') return false;
+                else if (!arrRow.containsKey(board[i][j]) && board[i][j] != '.') {
+                    arrRow.put(board[i][j], j);
+                }
+            }
+        }
+        return true;
+    }
+    public boolean isValidCol(char[][] board, Map<Character, Integer> arrCol) {
+        for (int j = 0; j < 9; j++) {
+            arrCol.clear();
+            for (int i = 0; i < 9; i++) {
+                if (arrCol.containsKey(board[i][j]) && board[i][j] != '.') return false;
+                else if (!arrCol.containsKey(board[i][j]) && board[i][j] != '.') {
+                    arrCol.put(board[i][j], j);
+                }
+            }
+        }
+        return true;
+    }
+
+//    public boolean isValidColumn(char[][] board, Map<Character, Integer> arrCol, int i, int j) {
+//        if (i == board[0].length) {
+//            j++;
+//            i = 0;
+//            arrCol.clear();
+//        }
+//        if (j == board[0].length) return true;
+//        if (board[i][j] == '.') {
+//            i++;
+//            return isValidRow(board, arrCol, i, j);
+//        }
+//        if (arrCol.containsKey(board[i][j]) && board[i][j] != '.') return false;
+//        else if (!arrCol.containsKey(board[i][j]) && board[i][j] != '.') {
+//            arrCol.put(board[i][j], j);
+//            i++;
+//        }
+//        return isValidRow(board, arrCol, i, j);
+//    }
+
+    public boolean checkSubBox(char[][] board, Map<Character, Integer> arrSubBox) {
+        for (int i = 0; i < 9; i += 3) {
+            arrSubBox.clear();
+            for (int j = 0; j < 9; j += 3) {
+                arrSubBox.clear();
+                for (int k = 0; k < 9; k++) {
+                    int r = j + k % 3;
+                    int c = i + k / 3;
+                    if (arrSubBox.containsKey(board[r][c]) && board[r][c] != '.') return false;
+                    else if (!arrSubBox.containsKey(board[r][c]) && board[r][c] != '.') {
+                        arrSubBox.put(board[r][c], r);
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
